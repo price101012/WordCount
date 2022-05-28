dc82d3c8f0a4968c4193bad4efc83f7a77bb3e97
zengcheng <1359666023@qq.com>
2022-05-28 21:59:10 +0800

    second submit

--- WordCount/WordCount.cpp
+++ WordCount/WordCount.cpp
@@ -2,31 +2,31 @@
 #include <string.h>
 int main (int argc, char *argv[])
 {
-    // argc 表示参数个数, *argv[]指针数组, 每一个指针指向一个字符串
+   
     FILE *fp;
-    int cnt = 0; // 统计数
-    if ((fp=fopen(argv[2], "r")) == NULL)
+    int count = 0;  
+    if ((fp=fopen(argv[2], "r"))==NULL)
     {
-        puts("error!\n");
+        puts("请先输入要检测的文本!\n");
         return 0;
     }
-    if (argv[1][1] == 'w') // 统计单词
+    if (argv[1][1]=='w') 
     {
-        char s[1024];
-        while (fscanf(fp, "%s", s) != EOF)
+        char ars[1024];
+        while (fscanf(fp, "%s", ars)!=EOF)
         {
-            cnt ++;
-            for (int i = 1; i < strlen(s) - 1; i ++)
-                if (s[i] == ',' && s[i - 1] != ',' && s[i + 1] != ',') 
-                    cnt ++;
+            count ++;
+            for (int i=1; i < strlen(ars) - 1; i ++)
+            	if(ars[i-1]!=','&&ars[i]==','&&ars[i+1]!=',') 
+                    count++;
         }
-        printf("单词数=%d\n", cnt);
+        printf("单词数=%d\n", count);
     }
-    else if (argv[1][1] == 'c') // 统计字符
+    else if (argv[1][1]=='c') 
     {
-        char c;
-        while ((c = fgetc(fp)) != EOF) cnt ++;
-        printf("字符数=%d", cnt);
+        char kato;
+        while ((kato = fgetc(fp)) != EOF) count++;
+        printf("字符数=%d", count);
     }
     fclose(fp);
     return 0;
